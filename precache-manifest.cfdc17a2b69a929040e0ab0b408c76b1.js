2f6a625064bcb9ef5171efbf94ef6e9a7c475db1
Rajesh Bhartia <rbhartiamuk@gmail.com>
2020-02-17 17:09:30 +0600

    Updates

---
+++ precache-manifest.cfdc17a2b69a929040e0ab0b408c76b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6f1ebee6fe8fbacdaf6292ee29d837",
+    "url": "/Rajeshbhartia/ReactBeautifulDragDropCopy.git/index.html"
+  },
+  {
+    "revision": "3211157bd599d3b425fc",
+    "url": "/Rajeshbhartia/ReactBeautifulDragDropCopy.git/static/css/main.984b7ab9.chunk.css"
+  },
+  {
+    "revision": "ead24d4a742c34670ca7",
+    "url": "/Rajeshbhartia/ReactBeautifulDragDropCopy.git/static/js/2.0f3c886c.chunk.js"
+  },
+  {
+    "revision": "131d0c04a34f6f3189b1c7d3e7416891",
+    "url": "/Rajeshbhartia/ReactBeautifulDragDropCopy.git/static/js/2.0f3c886c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3211157bd599d3b425fc",
+    "url": "/Rajeshbhartia/ReactBeautifulDragDropCopy.git/static/js/main.e7261bf2.chunk.js"
+  },
+  {
+    "revision": "91674ea141fe33401feb",
+    "url": "/Rajeshbhartia/ReactBeautifulDragDropCopy.git/static/js/runtime-main.ad5e50c4.js"
+  }
+]);
