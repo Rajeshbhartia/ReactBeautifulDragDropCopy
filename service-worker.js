b9f24c3b63841e450235d475bc8e7900d9629aea
unknown <rbhartiamuk@gmail.com>
2020-02-17 22:15:04 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactBeautifulDragDropCopy.git/precache-manifest.b8eb94205833bbec68e591f41f9d9929.js"
+  "/ReactBeautifulDragDropCopy/precache-manifest.8ddfb2d19169f7c083ae488097f08241.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactBeautifulDragDropCopy.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactBeautifulDragDropCopy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
