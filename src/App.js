ee267dc32a13d0d1ca06e763276a70296335b9f2
Rajesh Bhartia <rbhartiamuk@gmail.com>
2020-02-17 16:53:17 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import DragAndDropRBD from './DragAndDropRBD';
 
 class App extends Component {
+  // componentDidMount() {
+  //   let location = window.location.href
+  //   console.log(location)
+
+  //   document.addEventListener('DOMContentLoaded', function() {
+  //     // window.location.replace("http://www.google.com");
+  //     // window.location.replace("http://localhost:3000/#");
+  //  }, false);
+  // }
+
+  // componentDidMount() {
+  //   var xhttp = new XMLHttpRequest();
+  //   xhttp.onreadystatechange = function() {
+  //     if (this.readyState == 4 && this.status == 200) {
+  //       document.getElementById("demo").innerHTML =
+  //       this.responseText;
+  //     }
+  //   };
+  //   xhttp.open("GET", "https://myaccount-dev.synopi.com:13443/signin", true);
+  //   xhttp.send();
+  // }
+  
+  
   render() {
     return (
-      <div>
+      <div id="demo">
         <DragAndDropRBD />
+        {/* hello */}
+
       </div>
     );
   }
---
+++ src/xmlhttpquery.js
@@ -0,0 +1,7 @@
+var XMLHttpRequest = require('xhr2');
+
+function x(){
+    var xhttp = new XMLHttpRequest();
+    console.log("hello",xhttp)
+}
+let con = x()
